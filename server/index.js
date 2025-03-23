731e024e0d8f4423f7535407703005e1d27ce90e
Gaurav Jain <gauravjain.0184@gmail.com>
2025-03-23 21:55:08 +0530

    upload to git

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    app.listen(PORT, () => console.log(`✅ Server running on port: ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY */
+    // const existingData = await KPI.find();
+    // if (existingData.length === 0) {
+    //   console.log("🌱 Seeding initial data...");
+    //   await KPI.insertMany(kpis);
+    //   await Product.insertMany(products);
+    //   await Transaction.insertMany(transactions);
+    //   console.log("✅ Data seeded successfully!");
+    // }
+  })
+  .catch((error) => console.log(`❌ MongoDB Error: ${error.message}`));
